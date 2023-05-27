37200dca02dab6814eae006252342bca22e7ccc8
NadMag <25124870+NadMag@users.noreply.github.com>
2023-05-27 13:08:18 +0300

    Changed mask to be uniform across channels

--- defenses.py
+++ defenses.py
@@ -189,7 +189,7 @@ class NeuralCleanse:
         - trigger: 
         """
         # randomly initialize mask and trigger in [0,1] - FILL ME
-        mask = torch.rand(self.dim, device=device)
+        mask = torch.rand((1, 1 , self.dim[2], self.dim[3]), device=device)
         mask.requires_grad = True
         trigger = torch.rand(self.dim, device=device)
         trigger.requires_grad = True
